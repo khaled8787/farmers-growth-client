bbb1926fc3ce06101203a71aad99a2488982d97a
khaled8787 <khaledmahmud01737@gmail.com>
2025-11-09 21:05:29 +0600

    setup vite react project with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-center mt-10 text-3xl font-bold text-green-700">
+      KrishiLink Client is Running 🌿
+    </div>
+  )
+}
+
+export default App;
